21e49cd9d98352cd8248fd8166ff8ae01c4cda6c
James W <>
2021-05-14 16:05:49 +0000

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,34 @@ import Header from './components/Header'
 import './App.css';
 
 function App() {
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets/")
+      .then(response => response.json())
+      .then(data => setPlanets(data.results))
+  }, [])
+
   return (
     <>
       <Header title="
       Star Wars compare-a-planet"/>
-      <main>
-        
+
+
+
+      <main className="input">
+      
+        <select className="menu" name="cars" id="cars">
+          {planets.map(planet => (<option value={planet.name}>{planet.name}</option>))}
+        </select>
+        <select className="menu" name="cars" id="cars">
+          {planets.map(planet => (<option value={planet.name}>{planet.name}</option>))}
+        </select>
       </main>
     </>
   );
 }
+
+
 
 export default App;
